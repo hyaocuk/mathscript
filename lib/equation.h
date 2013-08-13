69d456ff915de5f0d393c22f4df9829f656fb9f7
Terence Ng <pheotiman@gmail.com>
2013-08-13 22:19:01 +0800

    equation.h - add options for complex and real

--- lib/equation.h
+++ lib/equation.h
@@ -21,15 +21,37 @@
 #ifndef _EQUATION_H
 #define _EQUATION_H
 
+/*
+ * NO_COMPLEX = 0: use complex numbers
+ *    ...     = 1: use real numbers
+ */
+#define NO_COMPLEX 0
+
 #include<stdio.h>
 #include<math.h>
 #include<stdlib.h>
 
-typedef struct __roots_quadratic_equation
+#include"complex.h"
+
+#if NO_COMPLEX == 1
+
+typedef struct
+__roots_quadratic_equation
 {
 	double root1;
 	double root2;
 } roots_quadratic_equation;
+
+#elif NO_COMPLEX == 0
+
+typedef struct
+__roots_quadratic_equation
+{
+	complex_number *root1;
+	complex_number *root2;
+} roots_quadratic_equation;
+
+#endif /* NO_COMPLEX */
 
 /* evaluate part of the equation */
 double
@@ -40,7 +62,8 @@ double
 discriminant ( double, double, double );
 
 /* get the final roots */
-roots_quadratic_equation *solve_quadratic_equation ( double, double, double );
+roots_quadratic_equation
+*solve_quadratic_equation ( double, double, double );
 
 #endif /* _EQUATION_H */
 
