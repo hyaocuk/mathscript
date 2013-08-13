200dab81d0a7f70048e9f28d6cb42db6161a77ea
Terence Ng <pheotiman@gmail.com>
2013-08-13 22:21:46 +0800

    add equation_test.h

---
+++ test/equation_test.h
@@ -0,0 +1,28 @@
+/*
+ * equation.h - declaration for equation.c
+ *     
+ * Copyright (C) 2013  Terence Ng
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef _EQUATION_H
+#define _EQUATION_H
+
+/* define style of printing complex numbers */
+void
+print_complex_number ( double, double );
+
+#endif /* _EQUATION_H */
