bacc0acb827073c583886c3e5ceafac7bf018998
Terence Ng <pheotiman@gmail.com>
2013-08-14 13:58:55 +0800

    add mathlib.h

---
+++ lib/mathlib.h
@@ -0,0 +1,34 @@
+/*
+ * mathlib.h - declarations of different arithematic operations
+ *     
+ * Copyright (C) 2013  Terence Ng
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+#ifndef _MATHLIB_H
+#define _MATHLIB_H
+
+#include<stdio.h>
+#include<math.h>
+
+/* small unsigned integer factoring */
+unsigned int
+factoring ( unsigned int );
+
+/* TODO big unsigned integer factoring */
+
+#endif /* _MATHLIB_H */
